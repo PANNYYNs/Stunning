22c14bcb092adb9e8fd03f95b8408fcc4b40175f
kopanda-1107 <ko.panda@hotmail.com>
2021-02-03 12:07:42 +0700

    2 files modified

--- src/app/comment/comment.component.ts
+++ src/app/comment/comment.component.ts
@@ -1,27 +1,27 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { AngularFirestore } from '@angular/fire/firestore';
-import { MatSnackBar } from '@angular/material/snack-bar';
+import { Component, OnInit, Input } from "@angular/core";
+import { AngularFirestore } from "@angular/fire/firestore";
+import { MatSnackBar } from "@angular/material/snack-bar";
 
 @Component({
-  selector: 'app-comment',
-  templateUrl: './comment.component.html',
-  styleUrls: ['./comment.component.css']
+  selector: "app-comment",
+  templateUrl: "./comment.component.html",
+  styleUrls: ["./comment.component.css"]
 })
 export class CommentComponent implements OnInit {
-
   constructor(
     private firestore: AngularFirestore,
-      private sanck:MatSnackBar,
-  ) { }
+    private sanck: MatSnackBar
+  ) {}
   @Input() comment;
   @Input() email;
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
-  delete(){
-    this.firestore.collection("comments").doc(this.comment.commentid).delete()
-    this.sanck.open("Delete success","close")
+  delete() {
+    this.firestore
+      .collection("comments")
+      .doc(this.comment.commentid)
+      .delete();
+    this.sanck.open("Delete success", "close");
   }
-
 }
