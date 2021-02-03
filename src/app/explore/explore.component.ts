5c8ca002ebaa29dbd2445c8fe544f53337f70f6d
kopanda-1107 <ko.panda@hotmail.com>
2021-02-03 12:52:10 +0700

    2 files modified

--- src/app/explore/explore.component.ts
+++ src/app/explore/explore.component.ts
@@ -1,69 +1,71 @@
-import { Component, OnInit } from '@angular/core';
-import { AngularFirestore } from '@angular/fire/firestore';
+import { Component, OnInit } from "@angular/core";
+import { AngularFirestore } from "@angular/fire/firestore";
 
 @Component({
-  selector: 'app-explore',
-  templateUrl: './explore.component.html',
-  styleUrls: ['./explore.component.css']
+  selector: "app-explore",
+  templateUrl: "./explore.component.html",
+  styleUrls: ["./explore.component.css"]
 })
 export class ExploreComponent implements OnInit {
-
-  constructor(
-    private firestore: AngularFirestore
-  ) { }
-  imageall ;
+  constructor(private firestore: AngularFirestore) {}
+  imageall;
   explore = "Explore";
   ngOnInit() {
-    this.firestore.collection("images").snapshotChanges().subscribe(val => {
-        this.imageall = val.map( e => {
+    this.firestore
+      .collection("images")
+      .snapshotChanges()
+      .subscribe(val => {
+        this.imageall = val.map(e => {
           return {
             id: e.payload.doc.id,
             ...e.payload.doc.data()
-            }
-          })
+          };
+        });
       });
   }
 
-  popular(){
+  popular() {
     this.explore = "Popular";
-    this.firestore.collection('images',votenumber=>votenumber.orderBy('votenumber','desc')).snapshotChanges().subscribe(
-      val => {
-        this.imageall = val.map( e => {
+    this.firestore
+      .collection("images", votenumber =>
+        votenumber.orderBy("votenumber", "desc")
+      )
+      .snapshotChanges()
+      .subscribe(val => {
+        this.imageall = val.map(e => {
           return {
             id: e.payload.doc.id,
             ...e.payload.doc.data()
-            }
-          })
-      }
-    );
+          };
+        });
+      });
   }
-  upcoimg(){
+  upcoimg() {
     this.explore = "Upcoimg";
-    this.firestore.collection('images',ref=>ref.orderBy('date','desc')).snapshotChanges().subscribe(
-      val => {
-        this.imageall = val.map( e => {
+    this.firestore
+      .collection("images", ref => ref.orderBy("date", "desc"))
+      .snapshotChanges()
+      .subscribe(val => {
+        this.imageall = val.map(e => {
           return {
             id: e.payload.doc.id,
             ...e.payload.doc.data()
-            }
-          })
-      }
-    );
+          };
+        });
+      });
   }
-  latest(){
-    this.explore = "Latest";
-    this.firestore.collection('images',ref=>ref.orderBy('date')).snapshotChanges().subscribe(
-      val => {
-        this.imageall = val.map( e => {
+  Oldest() {
+    this.explore = "Oldest";
+    this.firestore
+      .collection("images", ref => ref.orderBy("date"))
+      .snapshotChanges()
+      .subscribe(val => {
+        this.imageall = val.map(e => {
           return {
             id: e.payload.doc.id,
             ...e.payload.doc.data()
-            }
-          })
-      }
-    );
+          };
+        });
+      });
   }
-
-  
-
 }
